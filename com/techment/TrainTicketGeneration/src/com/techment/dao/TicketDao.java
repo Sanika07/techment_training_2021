3f4d8eab358534f13c0defbb3c0cd5d9151b59a2
Sanika Awasthi <sanikaa32@gmail.com>
2021-08-16 21:36:54 +0530

    Add files via upload

---
+++ com/techment/TrainTicketGeneration/src/com/techment/dao/TicketDao.java
@@ -0,0 +1,21 @@
+package com.techment.dao;
+
+import java.time.LocalDate;
+import java.util.Map;
+
+import com.techment.model.Passenger;
+import com.techment.model.Train;
+
+public interface TicketDao {
+	
+	Train findTrain(int tno);
+	Map<Passenger, Double> addPassenger(Train train);
+	double calcPassengerFare(Map<Passenger, Double> map);
+	String generatePNR(Train train,LocalDate ld);
+	void generateTicket(Train train,String Pnr,String date,Map<Passenger, Double> hashMap,Double TotalFare);
+	
+	public static final String  DRIVER_NAME="com.mysql.cj.jdbc.Driver";
+	public static final String URL="jdbc:mysql://localhost:3306/techment?autoReconnect=true&useSSL=false";
+	public static final String USERNAME="root";
+	public static final String PASSWORD="@admin123";
+}
