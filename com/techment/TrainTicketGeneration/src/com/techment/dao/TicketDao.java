e728fe9b5b880a2f8590ffded8ba765203373180
Sanika Awasthi <sanikaa32@gmail.com>
2021-08-16 21:39:39 +0530

    Update TicketDao.java

--- com/techment/TrainTicketGeneration/src/com/techment/dao/TicketDao.java
+++ com/techment/TrainTicketGeneration/src/com/techment/dao/TicketDao.java
@@ -17,5 +17,5 @@ public interface TicketDao {
 	public static final String  DRIVER_NAME="com.mysql.cj.jdbc.Driver";
 	public static final String URL="jdbc:mysql://localhost:3306/techment?autoReconnect=true&useSSL=false";
 	public static final String USERNAME="root";
-	public static final String PASSWORD="@admin123";
+	public static final String PASSWORD="mysql123";
 }
