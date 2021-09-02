09b744591864693ff81e89504ebdf3fe3cf5c1fe
Sanika Awasthi <sanikaa32@gmail.com>
2021-09-02 16:39:29 +0530

    Update DataInsertion.java

--- com/techment/JPA_Hibernate_Assignment/SecondQuestion/src/main/java/com/techment/DataInsertion.java
+++ com/techment/JPA_Hibernate_Assignment/SecondQuestion/src/main/java/com/techment/DataInsertion.java
@@ -4,7 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-public class OneToManyClientApp {
+public class DataInsertion {
 
 	public static void main(String[] args) {
 
@@ -13,22 +13,22 @@ public class OneToManyClientApp {
         em.getTransaction().begin();
         
         Author author = new Author();
-        author.setName("nishita");
+        author.setName("sanika");
         em.persist(author);
         
         Books bk1 = new Books();
         bk1.setISBN(1111);
-        bk1.setTitle("java");
-        bk1.setPrice(900);
+        bk1.setTitle("hibernate");
+        bk1.setPrice(700);
         em.persist(bk1);
         
         Books bk2 = new Books();
         bk2.setISBN(1112);
-        bk2.setTitle("python");
-        bk2.setPrice(1000);
+        bk2.setTitle("spring");
+        bk2.setPrice(800);
         em.persist(bk2);
         
-        System.out.println("====INSERTED====");
+        System.out.println("======INSERTED======");
         em.getTransaction().commit();
 
 	}
