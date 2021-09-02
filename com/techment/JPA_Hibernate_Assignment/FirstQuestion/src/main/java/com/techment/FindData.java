1da6317adf7de8ed2e049c47309c9a3eb9df0b6c
Sanika Awasthi <sanikaa32@gmail.com>
2021-09-02 16:32:33 +0530

    Update FindData.java

--- com/techment/JPA_Hibernate_Assignment/FirstQuestion/src/main/java/com/techment/FindData.java
+++ com/techment/JPA_Hibernate_Assignment/FirstQuestion/src/main/java/com/techment/FindData.java
@@ -4,7 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-public class Find {
+public class FindData {
 
 	public static void main(String[] args) {
 		
